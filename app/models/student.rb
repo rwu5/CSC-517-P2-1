bb65eefb39360a9de2aef90e8b554e8ad17f722f
Qingyan Wang <346829766@qq.com>
2019-10-07 21:38:18 -0400

    validations for librians and wishlist

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,6 +1,8 @@
 class Student < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true, length: {in: 6..15}
+  validates :education_level, presence: true
+  validates :university, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
